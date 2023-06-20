23b1e9b64cad119696697311975cd10da98fb491
pec35 <jakob.sigmund@gmail.com>
2023-06-20 21:59:13 +0200

    Kommentare hinzugefuegt

--- webseite/avlTree.js
+++ webseite/avlTree.js
@@ -4,6 +4,8 @@ const ctx = canvas.getContext('2d');
 let width = canvas.width = window.innerWidth;
 let height = canvas.height = window.innerHeight;
 
+
+// update canvas size when window size changes
 function updateCanvasDim() {
 	width = canvas.width = window.innerWidth;
 	height = canvas.height = window.innerHeight;
@@ -12,16 +14,19 @@ function updateCanvasDim() {
 
 window.addEventListener("resize", updateCanvasDim);
 
+// radius of the drawn nodes in pixels
 const size = 20;
+
+// speed controls of the animation
 const cPercent = 0.6;
 const cSpeed = 0.04;
 
-let testStore = [];
-
+// update Height property of a single node
 function updateHeight(x) {
 	x.height = 1 + Math.max(x.l.height, x.r.height);
 }
 
+// returns the next child on the longesth path to a leaf
 function longestChild(x) {
 	if (x.r.height > x.l.height) {
 		return x.r;
@@ -31,18 +36,24 @@ function longestChild(x) {
 	}
 }
 
+// getting a random integer betwenn to numbers
 function randInt(min, max) {
 	return min + Math.floor((max - min + 1) * Math.random());
 }
 
+// the magic of the avl tree
+// to understand the code one needs to know the avl tree algorithm
 function nodeRotate(x) {
+	// x, y, z are the nodes to rotate
 	let y = longestChild(x);
 	let z = longestChild(y);
+	// posX, posY, posZ are the current positions of x, y, z in the tree
 	let posX = getPos(x);
 	let posY = getPos(y);
 	let posZ = getPos(z);
 	let newNode = new Node(x.value);
 	let top = [];
+	// Create frames for tree above x
 	if (x.p !== null) {
 		if (x.p.l === x) {
 			x.p.l = new Node(null);
@@ -55,35 +66,40 @@ function nodeRotate(x) {
 			x.p.r = x;
 		}
 	}
+	// LL rotation
 	if (x.value > y.value && y.value > z.value) {
-
+		// untangle subtrees
 		x.l = new Node(null);
 		yR = y.r;
 		y.r = new Node(null);
 
+		// create NodeFrames for untangled subtrees
 		let frameX = new NodeFrame(x, posX, "#E6732C");
 		let frameY = new NodeFrame(y, posY, "#36A922");
 		let frameYR = new NodeFrame(yR, posRight(posY), "#FCDB23");
 
-
+		// Change the NodeFrames according the neccessary animation and store them for playback
 		frameX = frameX.setDest(posRight(posX));
 		frameY = frameY.copy();
 		frameYR = frameYR.copy();
 		let all = top.concat([frameX, frameY, frameYR])
 		frames.add(all);
 
+		// Change the NodeFrames according the neccessary animation and store them for playback
 		frameX = frameX.getEndFrame();
 		frameY = frameY.copy();
 		frameYR = frameYR.setDest(posLeft(posRight(posX)));
 		all = top.concat([frameX, frameY, frameYR])
 		frames.add(all);
 
+		// Change the NodeFrames according the neccessary animation and store them for playback
 		frameX = frameX.copy();
 		frameY = frameY.setDest(posX);
 		frameYR = frameYR.getEndFrame();
 		all = top.concat([frameX, frameY, frameYR])
 		frames.add(all);
 
+		// reattach the subtees to the new position
 		newNode.l = yR;
 		newNode.l.p = newNode;
 
@@ -97,9 +113,12 @@ function nodeRotate(x) {
 		x.r = newNode;
 		x.r.p = x;
 
+		// add NodeFrame of new tree
 		frames.add([new NodeFrame(root, [0, 0])]);
 	}
+	// LR rotation
 	else if (x.value > y.value && y.value < z.value) {
+		// untangle subtrees
 		let posZL = getPos(z.l);
 		let posZR = getPos(z.r);
 		x.l = new Node(null);
@@ -109,12 +128,14 @@ function nodeRotate(x) {
 		zR = z.r;
 		z.r = new Node(null);
 
+		// create NodeFrames for untangled subtrees
 		let frameX = new NodeFrame(x, posX, "#E6732C");
 		let frameY = new NodeFrame(y, posY, "#36A922");
 		let frameZ = new NodeFrame(z, posZ, "#FCDB23");
 		let frameZL = new NodeFrame(zL, posZL, "#FC23B5");
 		let frameZR = new NodeFrame(zR, posZR, "#3622F2");
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.setDest(posRight(posX));
 		frameY = frameY.copy();
 		frameZ = frameZ.copy();
@@ -123,6 +144,7 @@ function nodeRotate(x) {
 		let all = top.concat([frameX, frameY, frameZ, frameZL, frameZR]);
 		frames.add(all);
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.getEndFrame();
 		frameY = frameY.copy();
 		frameZ = frameZ.setDest(posX);
@@ -131,6 +153,7 @@ function nodeRotate(x) {
 		all = top.concat([frameX, frameY, frameZ, frameZL, frameZR]);
 		frames.add(all);
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.copy();
 		frameY = frameY.copy();
 		frameZ = frameZ.getEndFrame();
@@ -139,6 +162,7 @@ function nodeRotate(x) {
 		all = top.concat([frameX, frameY, frameZ, frameZL, frameZR]);
 		frames.add(all);
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.copy();
 		frameY = frameY.copy();
 		frameZ = frameZ.copy();
@@ -147,7 +171,7 @@ function nodeRotate(x) {
 		all = top.concat([frameX, frameY, frameZ, frameZL, frameZR]);
 		frames.add(all);
 
-
+		// reattach the subtees to the new position
 		newNode.l = zR;
 		newNode.l.p = newNode;
 		newNode.r = x.r;
@@ -161,29 +185,37 @@ function nodeRotate(x) {
 		y.r = zL;
 		y.r.p = y;
 
+		// add NodeFrame of new tree
 		frames.add([new NodeFrame(root, [0, 0])]);
 	}
+	// RR rotation
 	else if (x.value < y.value && y.value < z.value) {
+		// untangle subtrees
 		x.r = new Node(null);
 		yL = y.l;
 		y.l = new Node(null);
 
+		// create NodeFrames for untangled subtrees
 		let frameX = new NodeFrame(x, posX, "#E6732C");
 		let frameY = new NodeFrame(y, posY, "#36A922");
 		let frameYL = new NodeFrame(yL, posLeft(posY), "#FCDB23");
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.setDest(posLeft(posX));
 		frameY = frameY.copy();
 		frameYL = frameYL.copy();
 		let all = top.concat([frameX, frameY, frameYL])
 		frames.add(all);
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.getEndFrame();
 		frameY = frameY.copy();
 		frameYL = frameYL.setDest(posRight(posLeft(posX)));
 		all = top.concat([frameX, frameY, frameYL])
 		frames.add(all);
 
+
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.copy();
 		frameY = frameY.setDest(posX);
 		frameYL = frameYL.getEndFrame();
@@ -191,6 +223,7 @@ function nodeRotate(x) {
 		frames.add(all);
 
 
+		// reattach the subtees to the new position
 		newNode.l = x.l;
 		newNode.r = yL;
 
@@ -203,9 +236,12 @@ function nodeRotate(x) {
 		newNode.r.p = newNode;
 		z.p = x;
 
+		// add NodeFrame of new tree
 		frames.add([new NodeFrame(root, [0, 0])]);
 	}
+	// RL roatation
 	else if (x.value < y.value && y.value > z.value) {
+		// untangle subtrees
 		let posZL = getPos(z.l);
 		let posZR = getPos(z.r);
 		x.r = new Node(null);
@@ -215,12 +251,14 @@ function nodeRotate(x) {
 		zR = z.r;
 		z.r = new Node(null);
 
+		// create NodeFrames for untangled subtrees
 		let frameX = new NodeFrame(x, posX, "#E6732C");
 		let frameY = new NodeFrame(y, posY, "#36A922");
 		let frameZ = new NodeFrame(z, posZ, "#FCDB23");
 		let frameZL = new NodeFrame(zL, posZL, "#FC23B5");
 		let frameZR = new NodeFrame(zR, posZR, "#3622F2");
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.setDest(posLeft(posX));
 		frameY = frameY.copy();
 		frameZ = frameZ.copy();
@@ -229,6 +267,7 @@ function nodeRotate(x) {
 		let all = top.concat([frameX, frameY, frameZ, frameZL, frameZR]);
 		frames.add(all);
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.getEndFrame();
 		frameY = frameY.copy();
 		frameZ = frameZ.setDest(posX);
@@ -237,6 +276,7 @@ function nodeRotate(x) {
 		all = top.concat([frameX, frameY, frameZ, frameZL, frameZR]);
 		frames.add(all);
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.copy();
 		frameY = frameY.copy();
 		frameZ = frameZ.getEndFrame();
@@ -245,6 +285,7 @@ function nodeRotate(x) {
 		all = top.concat([frameX, frameY, frameZ, frameZL, frameZR]);
 		frames.add(all);
 
+		// Change the NodeFrames according the necessary animation and store them for playback
 		frameX = frameX.copy();
 		frameY = frameY.copy();
 		frameZ = frameZ.copy();
@@ -253,7 +294,7 @@ function nodeRotate(x) {
 		all = top.concat([frameX, frameY, frameZ, frameZL, frameZR]);
 		frames.add(all);
 
-
+		// reattach the subtees to the new position
 		newNode.r = zL;
 		newNode.r.p = newNode;
 		newNode.l = x.l;
@@ -267,20 +308,24 @@ function nodeRotate(x) {
 		y.l = zR;
 		y.l.p = y;
 
+		// add NodeFrame of new tree
 		frames.add([new NodeFrame(root, [0, 0])]);
 	}
+	// update heights after rotation
 	updateHeight(newNode);
 	updateHeight(y);
 	updateHeight(z);
 	updateHeight(x);
 }
 
+// calculate pixel coordinates from a position
 function cordinates(pos) {
 	let x = width * (pos[1] + 0.5) / Math.pow(2, pos[0]);
 	let y = 40 + 60 * pos[0];
 	return [x, y];
 }
 
+// draw node to the canvas
 function drawPartNode(node, part, oldPos, newPos) {
 	let c0 = cordinates(oldPos);
 	let c1 = cordinates(newPos);
@@ -304,6 +349,7 @@ function drawPartNode(node, part, oldPos, newPos) {
 	return [x, y];
 }
 
+// draw line between nodes to the canvas
 function drawLine(c1, c2) {
 	let x1 = c1[0];
 	let x2 = c2[0];
@@ -322,14 +368,17 @@ function drawLine(c1, c2) {
 }
 
 
+// get position of left node from position of current node
 function posLeft(pos) {
 	return [pos[0] + 1, 2 * pos[1]];
 }
 
+// get position of right node from position of current node
 function posRight(pos) {
 	return [pos[0] + 1, 2 * pos[1] + 1];
 }
 
+// get position of parent node from position of current node
 function posUp(pos) {
 	if (pos[0] === 0) {
 		return null;
@@ -339,6 +388,7 @@ function posUp(pos) {
 	}
 }
 
+// get position from node object relative to root node
 function getPos(node) {
 	let k = 0;
 	let l = 0;
@@ -355,8 +405,16 @@ function getPos(node) {
 	return [k, l];
 }
 
+// the class for storing the binary tree
 class Node {
+	// properties:
+	// this.l: left Node
+	// this.r: right Node
+	// this.height: height of subtree starting form node this
+	// this.p: parent Node
+	// this.color: the color for canvas drawing
 
+	// we assume that we only create nodes from number values
 	constructor(value) {
 		this.value = value;
 		if (this.value !== null) {
@@ -376,6 +434,7 @@ class Node {
 		this.p = null;
 	}
 
+	// a complete, indepenent copy of the subtree starting from this
 	copy() {
 		let cpy = new Node(this.value);
 		cpy.height = this.height;
@@ -389,6 +448,7 @@ class Node {
 		return cpy;
 	}
 
+	// returns next node on the path to the value 
 	next(value) {
 		if (value < this.value) {
 			return this.l;
@@ -398,7 +458,9 @@ class Node {
 		}
 	}
 
+	// inserting new value, and therefor new node into the tree
 	insert(newValue) {
+		// recursive insertion
 		if (this.value === null) {
 			this.value = newValue;
 			this.height = 1;
@@ -409,15 +471,20 @@ class Node {
 			this.r.height = 0;
 			this.l.p = this;
 			this.r.p = this;
+			// highlight insertion
 			frames.add([newHighlightFrame(root, [0, 0], "red", this)]);
 
 		}
 		else if (this.value !== newValue) {
+			// highlight node on path to insertion
 			frames.add([newHighlightFrame(root, [0, 0], "red", this)]);
+			// insert into subtree recursively
 			this.next(newValue).insert(newValue);
 			updateHeight(this);
 			frames.add([new NodeFrame(root, [0, 0])]);
+			// we check whether tree is unbalanced after insertion
 			if (1 < Math.abs(this.r.height - this.l.height)) {
+				// correct balance
 				nodeRotate(this);
 			}
 		}
@@ -426,6 +493,7 @@ class Node {
 		}
 	}
 
+	// returns node with corresponding value if it exists
 	find(value) {
 		if (this.value === null) {
 			return null;
@@ -439,6 +507,7 @@ class Node {
 		}
 	}
 
+	// draws the animation of the subtree of this onto the canvas by specifying the positions
 	drawPart(part, oldPos, newPos) {
 		let cParent = null;
 		if (this.value !== null) {
@@ -456,24 +525,7 @@ class Node {
 		return cParent;
 	}
 
-	getSmallest() {
-		if (this.value === null) {
-			return this.p;
-		}
-		else {
-			return this.l.getSmallest();
-		}
-	}
-
-	getBiggest() {
-		if (this.value === null) {
-			return this.p;
-		}
-		else {
-			return this.r.getBiggest();
-		}
-	}
-
+	//testing node and below for correctness
 	correct() {
 		if (this.value === null) {
 			return true;
@@ -487,6 +539,7 @@ class Node {
 		}
 	}
 
+	// specifying color for node and possibly subnodes depending on bool "all"
 	setColor(color, all) {
 		if (this.value !== null) {
 			this.color = color;
@@ -498,6 +551,8 @@ class Node {
 	}
 }
 
+//for testing purposes that the binary tree is correct
+let testStore = [];
 function testCorrect(place, node) {
 	let sample = node.copy();
 	testStore.push(sample);
@@ -508,7 +563,17 @@ function testCorrect(place, node) {
 	return true;
 }
 
+// the class for storing single animations and drawings onto the canvas
 class NodeFrame {
+	// properties:
+	// this.node: the subtree to draw
+	// this.oldPos: at the start the subtreee should be drawn here
+	// this.endPos: at the end the subtreee should be drawn here
+	// this.x: percentage number at which the animation is currently,
+	//		   if this.x === 1 is true the animation is complete
+	// this.start: percentage number at which the animation starts
+
+
 	constructor(node, oldPos, color) {
 		this.node = node.copy();
 		this.start = 1;
@@ -518,18 +583,22 @@ class NodeFrame {
 		if (color !== undefined) this.node.setColor(color, true);
 	}
 
+	// depending on the progress of this.x a different picture is drawn onto the canvas
 	drawNext() {
 		this.node.drawPart(this.x, this.oldPos, this.newPos);
 		if (this.x >= 1 || this.node.value === null) {
+			// here the animation
 			this.x = 1;
 			return true;
 		}
 		else {
+			// move this.x closer to completion
 			this.x += cSpeed;;
 			return false;
 		}
 	}
 
+	// make complete, independent copy fo the this
 	copy() {
 		let nodeFrame = new NodeFrame(this.node, this.oldPos);
 		nodeFrame.start = this.start;
@@ -539,6 +608,7 @@ class NodeFrame {
 		return nodeFrame;
 	}
 
+	// returns static nodeFrame from last picture of this
 	getEndFrame() {
 		let nodeFrame = this.copy();
 		nodeFrame.x = nodeFrame.start = 1;
@@ -546,6 +616,7 @@ class NodeFrame {
 		return nodeFrame;
 	}
 
+	// returns identical nodeFrame, except with a new destination
 	setDest(newPos) {
 		let nodeFrame = this.copy();
 		nodeFrame.x = 0;
@@ -555,6 +626,8 @@ class NodeFrame {
 	}
 }
 
+// a "second" constructor for highlighting single nodes
+// displays a static picture onto the canvas
 function newHighlightFrame(baseNode, pos, color, node) {
 	let nodeFrame = new NodeFrame(baseNode, pos, color);
 	nodeFrame.x = nodeFrame.start = 0;
@@ -564,7 +637,14 @@ function newHighlightFrame(baseNode, pos, color, node) {
 	return nodeFrame;
 }
 
+// container for all Node Frames
 class Frames {
+	// this.frames: array of arrays of nodeFrames
+	// this.play: bool for animation currently playing
+	// this.current: index of the nodeFrame currently animating
+	// this.cardinality: lenght of the array this.frames
+
+	// empty
 	constructor() {
 		this.frames = [];
 		this.play = false;
@@ -572,10 +652,12 @@ class Frames {
 		this.cardinality = 0;
 	}
 
+	// stopping animation
 	stop() {
 		this.play = false;
 	}
 
+	// play/continue animation
 	async go() {
 		if (this.play === false) {
 			this.play = true;
@@ -592,12 +674,16 @@ class Frames {
 		}
 	}
 
+	// add new array of nodeFrames to queue
 	add(frame) {
 		this.frames.push(frame);
 		this.cardinality += 1;
 	}
 
+	// draw only the i-th element of this.frames
 	draw(i) {
+		// because drawing onto the canvas happens asynchronously return Promise
+		// this is necessary, otherwise different nodeFrames try to paint the canvas at the same thime 
 		return new Promise((resolve, reject) => {
 			let loop = () => {
 				let finished = true;
@@ -614,6 +700,7 @@ class Frames {
 					return;
 				}
 				if (!finished) {
+					// draw next picture onto the canvas
 					requestAnimationFrame(loop);
 				}
 				else {
@@ -625,23 +712,23 @@ class Frames {
 	}
 }
 
+// frames stores the animation
 let frames = new Frames();
+// root stores the tree
 let root = new Node(null);
 
 const insertField = document.getElementById("inputField");
-insertField.value = "";
-
 const insertButton = document.getElementById("insertButton");
-insertButton.disabled = true;
-
 const randomField = document.getElementById("randomField");
-randomField.value = "";
 const randomButton = document.getElementById("randomButton");
-randomButton.disabled = true;
-
 const clearButton = document.getElementById("clearButton");
-
 const toggleBox = document.getElementById("toggleAnimation");
+
+// starting configuration
+insertField.value = "";
+insertButton.disabled = true;
+randomField.value = "";
+randomButton.disabled = true;
 toggleBox.checked = true;
 
 insertButton.addEventListener('click', handleInsert);
@@ -651,9 +738,11 @@ toggleBox.addEventListener('click', handleToggle);
 insertField.addEventListener('input', verifyInsertField);
 randomField.addEventListener('input', verifyRandomField);
 
+// regular expressions for text input
 const reInsert = /^\s*(\d|\d\d|\d\d\d)(\s+(\d|\d\d|\d\d\d))*\s*$/;
 const reRandom = /^\s*([1-9]|(1|2)\d|30)\s*$/;
 
+// verifying input against regular expressions
 function verifyInsertField(){
 	if(reInsert.test(insertField.value)){
 		insertField.style.color = "green";
@@ -665,6 +754,7 @@ function verifyInsertField(){
 	}
 }
 
+// verifying input against regular expressions
 function verifyRandomField(){
 	if(reRandom.test(randomField.value)){
 		randomField.style.color = "green";
@@ -676,6 +766,7 @@ function verifyRandomField(){
 	}
 }
 
+// setting animations accoring to checkbox 
 function animate(){
 	if (toggleBox.checked === true) {
 		frames.go();
@@ -685,6 +776,7 @@ function animate(){
 	}
 }
 
+// inserting values from input into tree and play animation
 function handleInsert() {
 	let arr = insertField.value.split(/\s+/);
 	for (let x of arr){
@@ -696,6 +788,7 @@ function handleInsert() {
 	insertButton.disabled = true;	
 }
 
+// delete whole tree
 function handleClear() {
 	root = new Node(null);
 	frames.stop();
@@ -705,6 +798,7 @@ function handleClear() {
 	ctx.clearRect(0, 0, width, height);
 }
 
+// inserting random numbers into tree
 function handleRandom() {
 	let x = parseInt(randomField.value);
 	for (let i = 0; i < x; i++) {
@@ -714,12 +808,7 @@ function handleRandom() {
 	animate();
 }
 
-function handleToggle() {
-	if (toggleBox.checked === false) {
-		skipAnimation();
-	}
-}
-
+// go directly to end of animation
 function skipAnimation() {
 	if (frames.cardinality !== 0) {
 		frames.stop();
@@ -727,3 +816,11 @@ function skipAnimation() {
 		setTimeout(() => { frames.go(); }, 100);
 	}
 }
+
+// switch animation accoring to checkbox
+function handleToggle() {
+	if (toggleBox.checked === false) {
+		skipAnimation();
+	}
+}
+
